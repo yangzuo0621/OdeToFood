c6a8c227ba43c062b96946e0abfe8f36c942020c
Zuo Yang <zuya@microsoft.com>
2018-01-08 16:33:29 +0800

    Using the Entity Framework

---
+++ Data/OdeToFoodDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Models;
+
+namespace OdeToFood.Data
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
